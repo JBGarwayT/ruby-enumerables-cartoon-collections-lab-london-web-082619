3f48b846eb437feea9c6cd7169f25cc5276800b5
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-07 16:12:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet (array)# code an argument here
 end
 
 def long_planeteer_calls (array)# code an argument here
-  if array.all? {|array| array.size == [1..4]}
+  if array.all? {|array| array.size <= 4}
     return false
   else
     return true
